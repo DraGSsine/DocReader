b53b21e2611bb2dfe89bbb93f580a6a982781009
Yassine Ouchen <ouchen606@gmail.com>
2023-10-24 14:09:09 +0100

    allow users to upload files

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,28 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+ 
+const f = createUploadthing();
+  
+export const ourFileRouter = {
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
+    .middleware(async ({ req }) => { 
+      const {getUser} = getKindeServerSession()
+      const user = getUser()
+      if(!user || !user.id) throw new Error('Unauthorizes')
+      return { userId:user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const createdFile = await db.file.create({
+        data:{
+          key:file.key,
+          name:file.name,
+          userId:metadata.userId,
+          url:file.url,
+          uploadStatus:"PROCESSING"
+        }
+      })
+    }),
+} satisfies FileRouter;
+ 
+export type OurFileRouter = typeof ourFileRouter;
--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,3 +1,5 @@
+import ChatWrapper from "@/components/ChatWrapper";
+import PdfRenderer from "@/components/PdfRenderer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
@@ -24,7 +26,6 @@ async function Page({ params }: PageProps) {
   });
   if (!file) notFound()
 
-  return 
   return (
     <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
       <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
@@ -32,12 +33,12 @@ async function Page({ params }: PageProps) {
         <div className='flex-1 xl:flex'>
           <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
             {/* Main area */}
-            <PdfRenderer url={file.url} />
+            <PdfRenderer/>
           </div>
         </div>
 
         <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
-          <ChatWrapper isSubscribed={plan.isSubscribed} fileId={file.id} />
+          <ChatWrapper/>
         </div>
       </div>
     </div>
